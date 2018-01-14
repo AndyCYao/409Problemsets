2d61234454c70135f8c925f68479a5af35d003f6
Andy Y <andyyao3@gmail.com>
2018-01-14 14:51:52 -0800

    built the adjacency list successfully

--- 10938Fleas/fleacircus.cpp
+++ 10938Fleas/fleacircus.cpp
@@ -1,16 +1,43 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
+
+vector<int>nodesVector;
+vector< vector<int> > adj;
+
+void CreateAdjacencyGraph(){
+/*
+    Assumes a 
+    vector<int>nodes;
+    vector< vector<int> > adj;
+    is declared globally
+    and the input stream is formatted like this
+    E // number of edges
+    1 2 //edge(1,2)
+    2 3 //edge(2,3)
+    ..so on
+*/
+    int nodes;
+    cin >> nodes;
+    //vector< vector<int> > adj(nodes, vector<int>(0));;
+    adj.resize(nodes, vector<int>(0));
+    //adj.assign(nodes, nodesVector);
+    //adj.pushback(nodes);
+
+    for(int edge = 0; edge < nodes - 1; edge++){
+        int nodeA, nodeB;
+        cin >> nodeA >> nodeB;
+        nodeA --; nodeB --; //our graph is zero based
+        adj[nodeA].push_back(nodeB);
+        adj[nodeB].push_back(nodeA);
+        //cout << nodeA << nodeB << endl;
+    }
+}
 
 int main(){
     freopen("input.txt","r", stdin);
-    int branches, cases;
-    cin >> branches;
-    for(int branch = 0; branch < branches - 1; branch++){
-        int a, b;
-        cin >> a >> b;
-        cout << a << b << endl;
-    }
+    int cases;
+    CreateAdjacencyGraph();
 
     cin >> cases;
     for(int x = 0; x < cases; x++){
