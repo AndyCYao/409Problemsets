ed4886686155b61a5a6039f2c2053e430ee405eb
Andy Y <andyyao3@gmail.com>
2018-02-21 23:56:15 -0800

    completed

--- a3/116UnidirectionTSP/116.cpp
+++ a3/116UnidirectionTSP/116.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 int travelMap[10][100];
-int path[10][100];
 int DP[10][100];
 int row, col;
 #define INT_MAX 2147483647 // 2^31 -1 
@@ -20,9 +20,11 @@ then check the min value in previous col. and print that out. thats the path.
 */
 
 void getLeastPath(){
+    
+    
     // get base case
     for(int r = 0; r < row; r++) DP[r][0] = travelMap[r][0];
-
+    int path[10][100] = {INT_MAX};
     for(int c = 1; c < col; c++){
         for(int r = 0; r < row ; r++){
             int tempIdx, tempVal,  cost;
@@ -33,7 +35,7 @@ void getLeastPath(){
                 if(tempVal < cost || (tempVal == cost && tempIdx < path[r][c - 1])){
                     cost       = tempVal;
                     DP[r][c]   = travelMap[r][c] + tempVal;
-                    path[r][c - 1] = tempIdx;
+                    path[r][c - 1] = tempIdx; // the spot [r][c-1] will lead to this row
                 }
             }
         }
@@ -50,21 +52,19 @@ void getLeastPath(){
         } 
     }
 
-    int printPath[col];
-    printPath[col - 1] = retracePt + 1;
+    printf("%d", retracePt + 1);
     for(int c = col - 2; c >= 0 ; c--){
         retracePt = path[retracePt][c];
-        printPath[c] = retracePt + 1;
+        printf(" %d", retracePt + 1);
     }
-    for(int i = 0; i < col; i++) cout << printPath[i] << " " ;
     cout << "\n" << bestCost << endl;
 }
 
 int main(){
-    freopen("input.in","r",stdin);
+    //freopen("input.in","r",stdin);
     while(cin >> row >> col){
-        for(int i = 0; i < row ; i ++){
-            for(int j = 0; j < col; j++){
+        for(int i = 0; i < row; i ++){
+            for(int j = col - 1; j >= 0; j--){
                 cin >> travelMap[i][j];
             }
         }
