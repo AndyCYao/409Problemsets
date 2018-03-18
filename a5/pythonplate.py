9d81cf04774dfee14a10f96f8529bafd8d96fc60
Naomi Zakimi <Naomi@d207-023-196-029.wireless.sfu.ca>
2018-03-17 18:02:32 -0700

    Boilerplate code for submission using python3

--- a5/pythonplate.py
+++ a5/pythonplate.py
@@ -1,3 +1,5 @@
+from sys import stdin
+
 # Defining boilerplate code for Python Submission
 def load_num():
     return int(stdin.readline())
@@ -6,8 +8,18 @@ def load_num():
 def load_pair():
     return tuple(map(int, stdin.readline().split()))
 
+# instanceNum is the number of problems.
 def load_instance():
     instanceNum = load_num()
     return [load_pair() for b in range(instanceNum)]
 
 if __name__ == '__main__':
+    n = load_num()
+
+    # While there is problem to solve
+    for i in range(n):
+        # Print result
+        print("Problem number {}".format(c+1))
+
+    # The command print is equivalent to stdout, but
+    # DO NOT USE CONCATENATION (e.g. print ("high ") + 5)
