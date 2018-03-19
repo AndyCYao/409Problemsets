1dd04bc31a67c5945d7b06c9fe3bec78ec9d31c6
ionwyn <ionwynsean@gmail.com>
2018-03-19 00:29:15 -0700

    10147 should be done, but theres a bug

--- a5/10147.py
+++ a5/10147.py
@@ -24,69 +24,71 @@ def load_instance_2(d):
         tmp = load_pair()
         d[tmp[0]][tmp[1]] = d[tmp[1]][tmp[0]] = -1.0
 
-def Distance(d, x, y, magicN):
-
-    # That magic number 751 tho
-    for i in range(1, magicN + 1):
-        d[i][i] = 0
-        for j in range(i+1, magicN + 1):
-            d[i][j] = d[j][i] = math.sqrt((x[i]-x[j])*(x[i]-x[j])+(y[i]-y[j])*(y[i]-y[j]))
-
-    return d
-
-def Prim(dist, magicN):
-    Free = [0]*(magicN+2)
-    d = [0]*(magicN+2)
-    Trace = [0] * 751
-
-    for i in range(1, magicN + 1):
-        Free[i] = True
-        d[i] = 100000
-
-    d[1] = 0
-
-    for j in range(1, magicN + 1):
-        u = 0
-        min = 100000
-        for k in range(1, magicN + 1):
-            if (Free[k] and (d[k] < min)):
-                u = k
-                min = d[k]
-        Free[u] = False
-
-        for l in range(1, magicN + 1):
-            print (type(Free[l]))
-            print (type(d[l]))
-            print (type(dist[u][l]))
-            if (Free[l] and (d[l] > dist[u][l])):
-                d[l] = dist[u][l]
-                Trace[l] = u
-
-    return Trace
-
-def Result(Trace, magicN):
-    non = True
-    for i in range(2,magicN + 1):
-        if (dist[i][Trace[u]] >= 0):
-            print ("{} {}\n".format(u, Trace[u]))
-            non = False
-    if (non):
-        print ("No new highways need")
-
 # Compiler looks at this first, as in main() in c++.
 # This, for Python at least, makes the compiler easy to detect EOF
 if __name__ == '__main__':
-    n = load_num()
-
-    x = [0]*751
-    y = [0]*751
-    distMatrix = [[0 for i in range(751)] for j in range(751)]
+    ney = load_num()
     # While there is problem to solve
-    for i in range(n):
-        # Print result
-        x,y, magicN = load_instance_1(x, y)
-        c = Distance(distMatrix, x, y, magicN)
-        dee = load_instance_2(c)
-        print (dee[0][0])
-        Trace = Prim(dee, magicN)
-        Result(Trace, magicN)
+    for r in range(ney):
+        n = load_num()
+        Free = [0]*(n+2)
+        d = [0]*(n+2)
+        x = [0]*755
+        y = [0]*755
+        distMatrix = [[0 for i in range(755)] for j in range(755)]
+        Trace = [0]*755
+
+        inf = 1000000
+        
+        for i in range(1, n+1):
+            x[i], y[i] = load_pair()
+
+        for i in range(1, n+1):
+            distMatrix[i][i] = 0
+            for j in range(i+1,n+1):
+                distMatrix[i][j] = distMatrix[j][i] = math.sqrt((x[i]-x[j])*(x[i]-x[j])+(y[i]-y[j])*(y[i]-y[j]))
+
+        m = load_num()
+
+        for i in range(1,m+1):
+            u, v = load_pair()
+            distMatrix[u][v] = distMatrix[v][u] = -1
+
+        for i in range(1, n+1):
+            Free[i] = True
+            d[i] = inf
+
+        d[1] = 0
+
+        for i in range(1,n+1):
+            u = 0
+            minD = inf
+
+            for j in range(1, n+1):
+                if (Free[j] and (d[j] < minD)):
+                    u = j
+                    minD = d[j]
+
+            for j in range(1,n+1):
+                if (Free[j] and (d[j] > distMatrix[u][j])):
+                    d[j] = distMatrix[u][j]
+                    Trace[j] = u
+
+        non = True
+
+        for v in range(2, n+1):
+            if (distMatrix[v][Trace[v]] >= 0):
+                print ("{} {}".format(v, Trace[v]))
+                non = False
+
+        if (non):
+            print ("No new highways need")
+
+        
+
+        
+
+
+
+
+
